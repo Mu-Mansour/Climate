1dee781aacba2b2283848a608bc36427b6974330
Mansour <v14mmotaleb11@gmail.com>
2025-10-07 15:53:52 +0300

    use tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import { AppRouter } from "./components/ui/AppRouter";
 import { ThemeProvider } from "./context/theme-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <ThemeProvider defaultTheme='dark'>
-      <AppRouter />
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
