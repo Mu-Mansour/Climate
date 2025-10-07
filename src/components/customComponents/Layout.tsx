13f520c391aec25a88b1382155b78e45679ab273
Mansour <v14mmotaleb11@gmail.com>
2025-10-07 15:55:31 +0300

    make changes to UI

--- src/components/customComponents/Layout.tsx
+++ src/components/customComponents/Layout.tsx
@@ -1,13 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "./Header";
 
 const Layout = () => {
   return (
     <div className='bg-gradient-to-br from-background to-muted'>
-      <header className='border-t backdrop-blur py-8 supports-[backdrop-filter]:bg-background/60'>
-        <div className='container mx-auto px-4 text-center text-gray-600'>
-          <p>ssssssssssssss</p>
-        </div>
-      </header>
+      <Header />
       <main className='min-h-screen container mx-auto px-4 py-8'>
         <Outlet />
       </main>
--- src/components/ui/AppRouter.tsx
+++ src/components/ui/AppRouter.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./Layout";
+import Layout from "../customComponents/Layout";
 
 const router = createBrowserRouter([
   {
